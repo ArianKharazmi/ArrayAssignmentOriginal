f6993bd5a58d53713d2b15d73a295ca0bbf96e72
Arian Kharazmi <100003892+ArianKharazmi@users.noreply.github.com>
2022-07-06 22:45:49 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.sql.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Size of First Array");
+        int input = scanner.nextInt();
+        System.out.println("Size of Second Array");
+        int input2 = scanner.nextInt();
+        Compare compare = new Compare();
+        int[] a = new int[input];
+        int[] b = new int[input2];
+        compare.arrayEqual(a,b);
+        System.out.println(compare.arrayEqual(a,b));
+    }
+}
